14750da920c8c51fa6e2181c41d959b0048cfc72
Iason Michailidis <iasonm57@gmail.com>
2018-09-04 18:20:27 +0300

    Initial commit

---
+++ IasonmPod/IasonmPod.swift
@@ -0,0 +1,16 @@
+//
+//  IasonmPod.swift
+//  IasonmPod
+//
+//  Created by Iason Michailidis on 04/09/2018.
+//  Copyright © 2018 Iason Michailidis. All rights reserved.
+//
+
+import Foundation
+
+public class IasonmPod {
+    public class func logToConsole(msg: String) {
+        print("Version 1.0.0: \(msg)" )
+    }
+}
+
